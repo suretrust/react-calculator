2335ffe473dd27677f7c2222c1e56b876dfabb85
Saheed <sdrfngg640@gmail.com>
2019-10-29 22:45:21 +0100

    Add button pannel component

---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,46 @@
+import Button from "./Button";
+import React from "react";
+
+class ButtonPanel extends React.Component {
+  renderButton(i) {
+    return <Button name={i} />;
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          {this.renderButton("AC")}
+          {this.renderButton("+/-")}
+          {this.renderButton("%")}
+          {this.renderButton(`÷`)}
+        </div>
+        <div>
+          {this.renderButton("7")}
+          {this.renderButton("8")}
+          {this.renderButton("9")}
+          {this.renderButton("X")}
+        </div>
+        <div>
+          {this.renderButton("4")}
+          {this.renderButton("5")}
+          {this.renderButton("6")}
+          {this.renderButton("-")}
+        </div>
+        <div>
+          {this.renderButton("1")}
+          {this.renderButton("2")}
+          {this.renderButton("3")}
+          {this.renderButton("+")}
+        </div>
+        <div>
+          {this.renderButton("0")}
+          {this.renderButton(".")}
+          {this.renderButton("=")}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ButtonPanel;
