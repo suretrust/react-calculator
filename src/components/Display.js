7c7c27578887435bce1c7f7cc67ed60d19ce8bc3
Saheed <sdrfngg640@gmail.com>
2019-10-29 22:45:33 +0100

    Add display component

---
+++ src/components/Display.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class Display extends React.Component {
+  render() {
+    return <div>{this.props.result}</div>;
+  }
+}
+
+Display.defaultProps = {
+  result: "0"
+};
+
+Display.propTypes = {
+  result: PropTypes.string
+};
+
+export default Display;
