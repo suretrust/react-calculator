83a19d65a64208ac5e351811cc4b3909e50f176c
Saheed <sdrfngg640@gmail.com>
2019-10-30 16:16:17 +0100

    Add SASS

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import './styles/css/index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
