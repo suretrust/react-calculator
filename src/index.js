fa50d91a361f4f57a970423f128bd7a0370803cf
Saheed <sdrfngg640@gmail.com>
2019-11-04 12:44:22 +0100

    Remove global document declaration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
-/* global document */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './styles/css/index.css'
+import './styles/css/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
