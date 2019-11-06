17d8b25c90c54c5f47de6ffbfc3e52be00770b96
Saheed <sdrfngg640@gmail.com>
2019-11-06 11:17:42 +0100

    Add handle click function

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,44 +1,51 @@
-/* eslint-disable class-methods-use-this */
 /* eslint-disable no-unused-vars */
+import PropTypes from 'prop-types';
 import React from 'react';
 import Button from './Button';
 
-class ButtonPanel extends React.Component {
-  render() {
-    return (
-      <section className="button-panel">
-        <div className="row">
-          <Button color="light-gray" name="AC" />
-          <Button color="light-gray" name="+/-" />
-          <Button color="light-gray" name="%" />
-          <Button name="÷" />
-        </div>
-        <div className="row">
-          <Button color="light-gray" name="7" />
-          <Button color="light-gray" name="8" />
-          <Button color="light-gray" name="9" />
-          <Button name="X" />
-        </div>
-        <div className="row">
-          <Button color="light-gray" name="4" />
-          <Button color="light-gray" name="5" />
-          <Button color="light-gray" name="6" />
-          <Button name="-" />
-        </div>
-        <div className="row">
-          <Button color="light-gray" name="1" />
-          <Button color="light-gray" name="2" />
-          <Button color="light-gray" name="3" />
-          <Button name="+" />
-        </div>
-        <div className="row">
-          <Button color="light-gray" wide={true} name="0" />
-          <Button color="light-gray" name="." />
-          <Button name="=" />
-        </div>
-      </section>
-    );
-  }
-}
+const ButtonPanel = ({ handleClick }) => {
+  return (
+    <section className="button-panel">
+      <div className="row">
+        <Button handleClick={handleClick} color="light-gray" name="AC" />
+        <Button handleClick={handleClick} color="light-gray" name="+/-" />
+        <Button handleClick={handleClick} color="light-gray" name="%" />
+        <Button handleClick={handleClick} name="÷" />
+      </div>
+      <div className="row">
+        <Button handleClick={handleClick} color="light-gray" name="7" />
+        <Button handleClick={handleClick} color="light-gray" name="8" />
+        <Button handleClick={handleClick} color="light-gray" name="9" />
+        <Button handleClick={handleClick} name="X" />
+      </div>
+      <div className="row">
+        <Button handleClick={handleClick} color="light-gray" name="4" />
+        <Button handleClick={handleClick} color="light-gray" name="5" />
+        <Button handleClick={handleClick} color="light-gray" name="6" />
+        <Button handleClick={handleClick} name="-" />
+      </div>
+      <div className="row">
+        <Button handleClick={handleClick} color="light-gray" name="1" />
+        <Button handleClick={handleClick} color="light-gray" name="2" />
+        <Button handleClick={handleClick} color="light-gray" name="3" />
+        <Button handleClick={handleClick} name="+" />
+      </div>
+      <div className="row">
+        <Button
+          handleClick={handleClick}
+          color="light-gray"
+          wide={true}
+          name="0"
+        />
+        <Button handleClick={handleClick} color="light-gray" name="." />
+        <Button handleClick={handleClick} name="=" />
+      </div>
+    </section>
+  );
+};
+
+ButtonPanel.propTypes = {
+  handleClick: PropTypes.func.isRequired
+};
 
 export default ButtonPanel;
